f090f695e3ebb43e969e4ab79e0df49396de865f
NielThakur <71279569+NielThakur@users.noreply.github.com>
2026-04-22 00:06:36 +0530

    Final EventCard.tsx

--- EventCard.tsx
+++ EventCard.tsx
@@ -1,4 +1,17 @@
+import { Ionicons } from "@expo/vector-icons";
 import { StyleSheet, Text, View } from "react-native";
+
+type Props = {
+  name: string;
+  category: string;
+  day: number;
+  time: string;
+  venue: string;
+  registrations: number;
+
+  isBookmarked: boolean;
+  onBookmark: () => void;
+};
 
 const styles = StyleSheet.create({
   card: {
@@ -6,6 +19,9 @@ const styles = StyleSheet.create({
     padding: 15,
     borderRadius: 10,
     marginBottom: 12,
+    overflow: "visible",
+
+    position: "relative",
   },
   title: {
     color: "white",
@@ -17,6 +33,14 @@ const styles = StyleSheet.create({
     marginTop: 5,
     fontSize: 14,
   },
+  bookmarkIcon: {
+    position: "absolute",
+    top: 15,
+    right: 15,
+    zIndex: 1000,
+    elevation: 1000,
+    padding: 8,
+  },
 });
 
 export default function EventCard({
@@ -26,15 +50,31 @@ export default function EventCard({
   time,
   venue,
   registrations,
-}: any) {
+  isBookmarked,
+  onBookmark,
+}: Props) {
   return (
-    <View style={styles.card}>
+    <View style={styles.card} pointerEvents="box-none">
+      <View style={styles.bookmarkIcon}>
+        <Ionicons
+          name={isBookmarked ? "bookmark" : "bookmark-outline"}
+          size={26}
+          color={isBookmarked ? "#e40606" : "#94a3b8"}
+          onPress={() => {
+            console.log("BOOKMARK CLICKED");
+            onBookmark();
+          }}
+          activeOpacity={0.6}
+          hitSlop={{ top: 15, bottom: 15, left: 15, right: 15 }}
+        />
+      </View>
+
       <Text style={styles.title}>{name}</Text>
-      <Text style={styles.body}>{"Category: " + category}</Text>
-      <Text style={styles.body}>{"Day: " + day}</Text>
-      <Text style={styles.body}>{"Time: " + time}</Text>
-      <Text style={styles.body}>{"Venue: " + venue}</Text>
-      <Text style={styles.body}>{"Registrations: " + registrations}</Text>
+      <Text style={styles.body}>Category: {category}</Text>
+      <Text style={styles.body}>Day: {day}</Text>
+      <Text style={styles.body}>Time: {time}</Text>
+      <Text style={styles.body}>Venue: {venue}</Text>
+      <Text style={styles.body}>Registrations: {registrations}</Text>
     </View>
   );
 }
